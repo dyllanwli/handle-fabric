7d80c49412d9a57c2004ec9ec95ce651c4d8c54a
lidiya <lidiya@veredholdings.com>
2018-09-07 15:20:32 +0800

    add 1.0

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # upgrade-fabric
 Upgrade Hyperledger Fabric Network; Simplifiy network upgrade
+
+- follow the official documention to download target version binaries file into bin/bin-$VERSION
--- fabric-samples-release-1.0/first-network/byfn.sh
+++ fabric-samples-release-1.0/first-network/byfn.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #
 # Copyright IBM Corp All Rights Reserved
 #
@@ -28,49 +29,45 @@
 
 # prepending $PWD/../bin to PATH to ensure we are picking up the correct binaries
 # this may be commented out to resolve installed version of tools if desired
-export PATH=${PWD}/../../bin/bin-1.1:${PWD}:$PATH
+export PATH=${PWD}/../../bin/bin-1.0:${PWD}:$PATH
 export FABRIC_CFG_PATH=${PWD}
 
 # Print the usage message
 function printHelp () {
   echo "Usage: "
-  echo "  byfn.sh up|down|restart|generate|upgrade [-c <channel name>] [-t <timeout>] [-d <delay>] [-f <docker-compose-file>] [-s <dbtype>] [-i <imagetag>]"
+  echo "  byfn.sh -m up|down|restart|generate [-c <channel name>] [-t <timeout>] [-d <delay>] [-f <docker-compose-file>] [-s <dbtype>] [-i <imagetag>]"
   echo "  byfn.sh -h|--help (print this message)"
-  echo "    <mode> - one of 'up', 'down', 'restart' or 'generate'"
+  echo "    -m <mode> - one of 'up', 'down', 'restart' or 'generate'"
   echo "      - 'up' - bring up the network with docker-compose up"
   echo "      - 'down' - clear the network with docker-compose down"
   echo "      - 'restart' - restart the network"
   echo "      - 'generate' - generate required certificates and genesis block"
-  echo "      - 'upgrade'  - upgrade the network from v1.0.x to v1.1"
   echo "    -c <channel name> - channel name to use (defaults to \"mychannel\")"
-  echo "    -t <timeout> - CLI timeout duration in seconds (defaults to 10)"
+  echo "    -t <timeout> - CLI timeout duration in microseconds (defaults to 10000)"
   echo "    -d <delay> - delay duration in seconds (defaults to 3)"
   echo "    -f <docker-compose-file> - specify which docker-compose file use (defaults to docker-compose-cli.yaml)"
   echo "    -s <dbtype> - the database backend to use: goleveldb (default) or couchdb"
-  echo "    -l <language> - the chaincode language: golang (default) or node"
-  echo "    -i <imagetag> - the tag to be used to launch the network (defaults to \"latest\")"
+  echo "    -i <imagetag> - pass the image tag to launch the network using the tag: 1.0.1, 1.0.2, 1.0.3, 1.0.4 (defaults to latest)"
   echo
   echo "Typically, one would first generate the required certificates and "
   echo "genesis block, then bring up the network. e.g.:"
   echo
-  echo "	byfn.sh generate -c mychannel"
-  echo "	byfn.sh up -c mychannel -s couchdb"
-  echo "        byfn.sh up -c mychannel -s couchdb -i 1.1.0-alpha"
-  echo "	byfn.sh up -l node"
-  echo "	byfn.sh down -c mychannel"
-  echo "        byfn.sh upgrade -c mychannel"
+  echo "	byfn.sh -m generate -c mychannel"
+  echo "	byfn.sh -m up -c mychannel -s couchdb"
+  echo "	byfn.sh -m up -c mychannel -s couchdb -i 1.0.6"
+  echo "	byfn.sh -m down -c mychannel"
   echo
   echo "Taking all defaults:"
-  echo "	byfn.sh generate"
-  echo "	byfn.sh up"
-  echo "	byfn.sh down"
+  echo "	byfn.sh -m generate"
+  echo "	byfn.sh -m up"
+  echo "	byfn.sh -m down"
 }
 
 # Ask user for confirmation to proceed
 function askProceed () {
-  read -p "Continue? [Y/n] " ans
+  read -p "Continue (y/n)? " ans
   case "$ans" in
-    y|Y|"" )
+    y|Y )
       echo "proceeding ..."
     ;;
     n|N )
@@ -107,46 +104,8 @@ function removeUnwantedImages() {
   fi
 }
 
-# Versions of fabric known not to work with this release of first-network
-BLACKLISTED_VERSIONS="^1\.0\. ^1\.1\.0-preview ^1\.1\.0-alpha"
-
-# Do some basic sanity checking to make sure that the appropriate versions of fabric
-# binaries/images are available.  In the future, additional checking for the presence
-# of go or other items could be added.
-function checkPrereqs() {
-  # Note, we check configtxlator externally because it does not require a config file, and peer in the
-  # docker image because of FAB-8551 that makes configtxlator return 'development version' in docker
-  LOCAL_VERSION=$(configtxlator version | sed -ne 's/ Version: //p')
-  DOCKER_IMAGE_VERSION=$(docker run --rm hyperledger/fabric-tools:$IMAGETAG peer version | sed -ne 's/ Version: //p'|head -1)
-
-  echo "LOCAL_VERSION=$LOCAL_VERSION"
-  echo "DOCKER_IMAGE_VERSION=$DOCKER_IMAGE_VERSION"
-
-  if [ "$LOCAL_VERSION" != "$DOCKER_IMAGE_VERSION" ] ; then
-     echo "=================== WARNING ==================="
-     echo "  Local fabric binaries and docker images are  "
-     echo "  out of  sync. This may cause problems.       "
-     echo "==============================================="
-  fi
-
-  for UNSUPPORTED_VERSION in $BLACKLISTED_VERSIONS ; do
-     echo "$LOCAL_VERSION" | grep -q $UNSUPPORTED_VERSION
-     if [ $? -eq 0 ] ; then
-       echo "ERROR! Local Fabric binary version of $LOCAL_VERSION does not match this newer version of BYFN and is unsupported. Either move to a later version of Fabric or checkout an earlier version of fabric-samples."
-       exit 1
-     fi
-
-     echo "$DOCKER_IMAGE_VERSION" | grep -q $UNSUPPORTED_VERSION
-     if [ $? -eq 0 ] ; then
-       echo "ERROR! Fabric Docker image version of $DOCKER_IMAGE_VERSION does not match this newer version of BYFN and is unsupported. Either move to a later version of Fabric or checkout an earlier version of fabric-samples."
-       exit 1
-     fi
-  done
-}
-
 # Generate the needed certificates, the genesis block and start the network.
 function networkUp () {
-  checkPrereqs
   # generate artifacts if they don't exist
   if [ ! -d "crypto-config" ]; then
     generateCerts
@@ -154,97 +113,31 @@ function networkUp () {
     generateChannelArtifacts
   fi
   if [ "${IF_COUCHDB}" == "couchdb" ]; then
-    IMAGE_TAG=$IMAGETAG docker-compose -f $COMPOSE_FILE -f $COMPOSE_FILE_COUCH up -d 2>&1
+      IMAGE_TAG=$IMAGETAG CHANNEL_NAME=$CHANNEL_NAME TIMEOUT=$CLI_TIMEOUT DELAY=$CLI_DELAY docker-compose -f $COMPOSE_FILE -f $COMPOSE_FILE_COUCH up -d 2>&1
   else
-    IMAGE_TAG=$IMAGETAG docker-compose -f $COMPOSE_FILE up -d 2>&1
+      IMAGE_TAG=$IMAGETAG CHANNEL_NAME=$CHANNEL_NAME TIMEOUT=$CLI_TIMEOUT DELAY=$CLI_DELAY docker-compose -f $COMPOSE_FILE up -d 2>&1
   fi
   if [ $? -ne 0 ]; then
     echo "ERROR !!!! Unable to start network"
+    docker logs -f cli
     exit 1
   fi
-  # now run the end to end script
-  docker exec cli scripts/script.sh $CHANNEL_NAME $CLI_DELAY $LANGUAGE $CLI_TIMEOUT
-  if [ $? -ne 0 ]; then
-    echo "ERROR !!!! Test failed"
-    exit 1
-  fi
+  docker logs -f cli
 }
-
-# Upgrade the network from v1.0.x to v1.1
-# Stop the orderer and peers, backup the ledger from orderer and peers, cleanup chaincode containers and images
-# and relaunch the orderer and peers with latest tag
-function upgradeNetwork () {
-  docker inspect  -f '{{.Config.Volumes}}' orderer.example.com |grep -q '/var/hyperledger/production/orderer'
-  if [ $? -ne 0 ]; then
-    echo "ERROR !!!! This network does not appear to be using volumes for its ledgers, did you start from fabric-samples >= v1.0.6?"
-    exit 1
-  fi
-
-  LEDGERS_BACKUP=./ledgers-backup
-
-  # create ledger-backup directory
-  mkdir -p $LEDGERS_BACKUP
-
-  export IMAGE_TAG=$IMAGETAG
-  if [ "${IF_COUCHDB}" == "couchdb" ]; then
-      COMPOSE_FILES="-f $COMPOSE_FILE -f $COMPOSE_FILE_COUCH"
-  else
-      COMPOSE_FILES="-f $COMPOSE_FILE"
-  fi
-
-  # removing the cli container
-  docker-compose $COMPOSE_FILES stop cli
-  docker-compose $COMPOSE_FILES up -d --no-deps cli
-
-  echo "Upgrading orderer"
-  docker-compose $COMPOSE_FILES stop orderer.example.com
-  docker cp -a orderer.example.com:/var/hyperledger/production/orderer $LEDGERS_BACKUP/orderer.example.com
-  docker-compose $COMPOSE_FILES up -d --no-deps orderer.example.com
-
-  for PEER in peer0.org1.example.com peer1.org1.example.com peer0.org2.example.com peer1.org2.example.com; do
-    echo "Upgrading peer $PEER"
-
-    # Stop the peer and backup its ledger
-    docker-compose $COMPOSE_FILES stop $PEER
-    docker cp -a $PEER:/var/hyperledger/production $LEDGERS_BACKUP/$PEER/
-
-    # Remove any old containers and images for this peer
-    CC_CONTAINERS=$(docker ps | grep dev-$PEER | awk '{print $1}')
-    if [ -n "$CC_CONTAINERS" ] ; then
-        docker rm -f $CC_CONTAINERS
-    fi
-    CC_IMAGES=$(docker images | grep dev-$PEER | awk '{print $1}')
-    if [ -n "$CC_IMAGES" ] ; then
-        docker rmi -f $CC_IMAGES
-    fi
-
-    # Start the peer again
-    docker-compose $COMPOSE_FILES up -d --no-deps $PEER
-  done
-
-  docker exec cli scripts/upgrade_to_v11.sh $CHANNEL_NAME $CLI_DELAY $LANGUAGE $CLI_TIMEOUT
-  if [ $? -ne 0 ]; then
-    echo "ERROR !!!! Test failed"
-    exit 1
-  fi
-}
-
 
 # Tear down running network
 function networkDown () {
-  docker-compose -f $COMPOSE_FILE -f $COMPOSE_FILE_COUCH down --volumes
   docker-compose -f $COMPOSE_FILE down --volumes
+  docker-compose -f $COMPOSE_FILE -f $COMPOSE_FILE_COUCH down --volumes
   # Don't remove the generated artifacts -- note, the ledgers are always removed
   if [ "$MODE" != "restart" ]; then
-    # Bring down the network, deleting the volumes
-    #Delete any ledger backups
-    docker run -v $PWD:/tmp/first-network --rm hyperledger/fabric-tools:$IMAGETAG rm -Rf /tmp/first-network/ledgers-backup
+    # Bring the containers down deleting their volumes
     #Cleanup the chaincode containers
     clearContainers
     #Cleanup images
     removeUnwantedImages
     # remove orderer block and other channel configuration transactions and certs
-    rm -rf channel-artifacts/*.block channel-artifacts/*.tx crypto-config ./org3-artifacts/crypto-config/ channel-artifacts/org3.json
+    rm -rf channel-artifacts/*.block channel-artifacts/*.tx crypto-config
     # remove the docker-compose yaml file that was customized to the example
     rm -f docker-compose-e2e.yaml
   fi
@@ -255,7 +148,7 @@ function networkDown () {
 # configuration
 function replacePrivateKey () {
   # sed on MacOSX does not support -i flag with a null extension. We will use
-  # 't' for our back-up's extension and delete it at the end of the function
+  # 't' for our back-up's extension and depete it at the end of the function
   ARCH=`uname -s | grep Darwin`
   if [ "$ARCH" == "Darwin" ]; then
     OPTS="-it"
@@ -311,15 +204,11 @@ function generateCerts (){
   echo "##########################################################"
   echo "##### Generate certificates using cryptogen tool #########"
   echo "##########################################################"
-
   if [ -d "crypto-config" ]; then
     rm -Rf crypto-config
   fi
-  set -x
   cryptogen generate --config=./crypto-config.yaml
-  res=$?
-  set +x
-  if [ $res -ne 0 ]; then
+  if [ "$?" -ne 0 ]; then
     echo "Failed to generate certificates..."
     exit 1
   fi
@@ -376,11 +265,8 @@ function generateChannelArtifacts() {
   echo "##########################################################"
   # Note: For some unknown reason (at least for now) the block file can't be
   # named orderer.genesis.block or the orderer will fail to launch!
-  set -x
   configtxgen -profile TwoOrgsOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
-  res=$?
-  set +x
-  if [ $res -ne 0 ]; then
+  if [ "$?" -ne 0 ]; then
     echo "Failed to generate orderer genesis block..."
     exit 1
   fi
@@ -388,11 +274,8 @@ function generateChannelArtifacts() {
   echo "#################################################################"
   echo "### Generating channel configuration transaction 'channel.tx' ###"
   echo "#################################################################"
-  set -x
   configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID $CHANNEL_NAME
-  res=$?
-  set +x
-  if [ $res -ne 0 ]; then
+  if [ "$?" -ne 0 ]; then
     echo "Failed to generate channel configuration transaction..."
     exit 1
   fi
@@ -401,11 +284,8 @@ function generateChannelArtifacts() {
   echo "#################################################################"
   echo "#######    Generating anchor peer update for Org1MSP   ##########"
   echo "#################################################################"
-  set -x
   configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
-  res=$?
-  set +x
-  if [ $res -ne 0 ]; then
+  if [ "$?" -ne 0 ]; then
     echo "Failed to generate anchor peer update for Org1MSP..."
     exit 1
   fi
@@ -414,12 +294,9 @@ function generateChannelArtifacts() {
   echo "#################################################################"
   echo "#######    Generating anchor peer update for Org2MSP   ##########"
   echo "#################################################################"
-  set -x
   configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate \
   ./channel-artifacts/Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
-  res=$?
-  set +x
-  if [ $res -ne 0 ]; then
+  if [ "$?" -ne 0 ]; then
     echo "Failed to generate anchor peer update for Org2MSP..."
     exit 1
   fi
@@ -432,7 +309,7 @@ OS_ARCH=$(echo "$(uname -s|tr '[:upper:]' '[:lower:]'|sed 's/mingw64_nt.*/window
 # timeout duration - the duration the CLI should wait for a response from
 # another container before giving up
 CLI_TIMEOUT=10
-# default for delay between commands
+#default for delay
 CLI_DELAY=3
 # channel name defaults to "mychannel"
 CHANNEL_NAME="mychannel"
@@ -440,36 +317,16 @@ CHANNEL_NAME="mychannel"
 COMPOSE_FILE=docker-compose-cli.yaml
 #
 COMPOSE_FILE_COUCH=docker-compose-couch.yaml
-# use golang as the default language for chaincode
-LANGUAGE=golang
 # default image tag
 IMAGETAG="latest"
 # Parse commandline args
-if [ "$1" = "-m" ];then	# supports old usage, muscle memory is powerful!
-    shift
-fi
-MODE=$1;shift
-# Determine whether starting, stopping, restarting or generating for announce
-if [ "$MODE" == "up" ]; then
-  EXPMODE="Starting"
-elif [ "$MODE" == "down" ]; then
-  EXPMODE="Stopping"
-elif [ "$MODE" == "restart" ]; then
-  EXPMODE="Restarting"
-elif [ "$MODE" == "generate" ]; then
-  EXPMODE="Generating certs and genesis block for"
-elif [ "$MODE" == "upgrade" ]; then
-  EXPMODE="Upgrading the network"
-else
-  printHelp
-  exit 1
-fi
-
-while getopts "h?m:c:t:d:f:s:l:i:" opt; do
+while getopts "h?m:c:t:d:f:s:i:" opt; do
   case "$opt" in
     h|\?)
       printHelp
       exit 0
+    ;;
+    m)  MODE=$OPTARG
     ;;
     c)  CHANNEL_NAME=$OPTARG
     ;;
@@ -481,20 +338,32 @@ while getopts "h?m:c:t:d:f:s:l:i:" opt; do
     ;;
     s)  IF_COUCHDB=$OPTARG
     ;;
-    l)  LANGUAGE=$OPTARG
-    ;;
     i)  IMAGETAG=`uname -m`"-"$OPTARG
     ;;
   esac
 done
 
+# Determine whether starting, stopping, restarting or generating for announce
+if [ "$MODE" == "up" ]; then
+  EXPMODE="Starting"
+  elif [ "$MODE" == "down" ]; then
+  EXPMODE="Stopping"
+  elif [ "$MODE" == "restart" ]; then
+  EXPMODE="Restarting"
+  elif [ "$MODE" == "generate" ]; then
+  EXPMODE="Generating certs and genesis block for"
+else
+  printHelp
+  exit 1
+fi
+
 # Announce what was requested
 
   if [ "${IF_COUCHDB}" == "couchdb" ]; then
         echo
-        echo "${EXPMODE} with channel '${CHANNEL_NAME}' and CLI timeout of '${CLI_TIMEOUT}' seconds and CLI delay of '${CLI_DELAY}' seconds and using database '${IF_COUCHDB}'"
+        echo "${EXPMODE} with channel '${CHANNEL_NAME}' and CLI timeout of '${CLI_TIMEOUT}' using database '${IF_COUCHDB}'"
   else
-        echo "${EXPMODE} with channel '${CHANNEL_NAME}' and CLI timeout of '${CLI_TIMEOUT}' seconds and CLI delay of '${CLI_DELAY}' seconds"
+        echo "${EXPMODE} with channel '${CHANNEL_NAME}' and CLI timeout of '${CLI_TIMEOUT}'"
   fi
 # ask for confirmation to proceed
 askProceed
@@ -502,17 +371,15 @@ askProceed
 #Create the network using docker compose
 if [ "${MODE}" == "up" ]; then
   networkUp
-elif [ "${MODE}" == "down" ]; then ## Clear the network
+  elif [ "${MODE}" == "down" ]; then ## Clear the network
   networkDown
-elif [ "${MODE}" == "generate" ]; then ## Generate Artifacts
+  elif [ "${MODE}" == "generate" ]; then ## Generate Artifacts
   generateCerts
   replacePrivateKey
   generateChannelArtifacts
-elif [ "${MODE}" == "restart" ]; then ## Restart the network
+  elif [ "${MODE}" == "restart" ]; then ## Restart the network
   networkDown
   networkUp
-elif [ "${MODE}" == "upgrade" ]; then ## Upgrade the network from v1.0.x to v1.1
-  upgradeNetwork
 else
   printHelp
   exit 1
